5bff3c634e32b469e02f03262968ba13733aae58
gkonto <geor.kontogiannis@gmail.com>
2025-08-13 23:37:53 +0300

    Enhancement in vector>

--- src/vector/Vector.hpp
+++ src/vector/Vector.hpp
@@ -76,8 +76,80 @@ public:
     {
         delete [] elems;
     }
+    void push_back(const_reference val)
+    {
+        if (full())
+            grow();
+        elems[size()] = val;
+        ++nelems;
+    }
+    void push_back(T &&val)
+    {
+        if (full())
+            grow();
+        elems[size()] = std::move(val);
+        ++nelems;
+    }
+    template<class ...Args>
+    reference emplace_back(Args &&...args)
+    {
+        if (full())
+            grow();
+        elems[size()] = value_type(std::forward<Args>(args)...);
+        ++nelems;
+        return back();
+    }
+    template<class It>
+    iterator insert(const_iterator pos, It first, It last)
+    {
+        iterator pos_ = const_cast<iterator>(pos);
+        const std::size_t remaining = capacity() - size();
+        const std::size_t n = std::distance(first, last);
+        if (remaining < n)
+        {
+            auto index = std::distance(begin(), pos_);
+            resize(capacity() + n - remaining);
+            pos_ = std::next(begin(), index);
+        }
+        std::copy_backward(pos_, end(), end() + n);
+        std::copy(first, last, pos_);
+        nelems += n;
+        return pos_;
+    }
+    iterator erase(const_iterator pos)
+    {
+        iterator pos_ = const_char<iterator>(pos);
+        if (pos_ == end()) return pos_;
+        std::copy(std::next(pos_), end(), pos_);
+        *std::prev(end()) = {};
+        --nelems;
+        return pos_;
+    }
 private:
     bool full() const { return size() == capacity(); }
+    void grow()
+    {
+        resize(capacity() ? capacity() * 2 : 16);
+    }
+    void resize(size_type new_cap)
+    {
+        if (new_cap <= capacity()) return;
+        auto p = new T[new_cap];
+        if constexpr(std::is_nothrow_move_assignable_v<T>)
+        {
+            std::move(begin(), end(), p);
+        } else try
+        {
+            std::copy(begin(), end(), p);
+        } catch (...)
+        {
+            delete[]p;
+            throw;
+        }
+        delete []elems;
+        elems = p;
+        cap = new_cap;
+    }
 };
 
 
